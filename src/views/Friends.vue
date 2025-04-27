8e5bdff2e50752c256a85c0b5579c2ef0a0952d9
Hope52411 <xuexiangze666@gmail.com>
2025-04-27 06:01:09 +0100

    Improve the interface of the mobile end

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -143,7 +143,7 @@ export default {
 
 
 .friends-container {
-  width: 85%;
+  width: 95%;
   max-width: 700px;
   background: rgba(255, 255, 255, 0.9);
   padding: 30px;
@@ -158,7 +158,7 @@ export default {
 }
 .friends-section {
   margin-top: 20px;
-  width: 85%;
+  width: 95%;
   max-width: 700px;
   background: rgba(255, 255, 255, 0.9);
   padding: 30px;
