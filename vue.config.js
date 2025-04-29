26568095605b2bfa868c25478a94e76fa40042cc
Hope52411 <xuexiangze666@gmail.com>
2025-04-29 04:21:14 +0100

    save before adding pwa support

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,14 @@ module.exports = defineConfig({
   publicPath: '/', 
   devServer: {
     historyApiFallback: true
+  },
+  pwa: {
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
   }
 });
